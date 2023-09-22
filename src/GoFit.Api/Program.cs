af81b1459668242c3828a0cff8426163f267ea07
Igor Jorge <igorjplino@gmail.com>
2023-09-22 09:20:55 +0100

    Added basics configurations for swagger

--- src/GoFit.Api/Program.cs
+++ src/GoFit.Api/Program.cs
@@ -1,16 +1,21 @@
 using FastEndpoints;
+using FastEndpoints.Swagger;
 using GoFit.Application;
 using GoFit.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddFastEndpoints();
+
+builder.Services.SwaggerDocument(o =>
+{
+    o.DocumentSettings = s =>
+    {
+        s.Title= "API GoFit";
+        s.Version= "v1";
+    };
+});
 
 // Services containers
 builder.Services.AddApplicationServices();
@@ -19,17 +24,15 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
 app.UseFastEndpoints();
 
-app.MapControllers();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerGen();
+}
 
 app.Run();
