aa6310767e23609af69ba284cf4a8e8674bb9107
Igor Jorge <igorjplino@gmail.com>
2023-09-25 15:44:33 +0100

    Added new entities WorkoutTracking and WorkoutSetTracking

---
+++ src/GoFit.Domain/Entities/WorkoutSetTracking.cs
@@ -0,0 +1,11 @@
+﻿using GoFit.Domain.Common;
+
+namespace GoFit.Domain.Entities;
+public class WorkoutSetTracking : BaseEntity
+{
+    public Guid WorkoutTrackingId { get; set; }
+    public WorkoutTracking WorkoutTracking { get; set; }
+    public int Repetitions { get; set; }
+    public float Weight { get; set; }
+    public int Order { get; set; }
+}
---
+++ src/GoFit.Domain/Entities/WorkoutTracking.cs
@@ -0,0 +1,13 @@
+﻿using GoFit.Domain.Common;
+
+namespace GoFit.Domain.Entities;
+
+public class WorkoutTracking : BaseEntity
+{
+    public Guid WorkoutId { get; set; }
+    public Workout Workout { get; set; }
+    public DateTime StartWorkoutDate { get; set; }
+    public DateTime EndWorkoutDate { get; set; }
+    public string Note { get; set; }
+    public ICollection<WorkoutSetTracking> Sets { get; set; }
+}
