517ade5029f2fd7ee0328eef6d32bae8003b3c93
Igor Jorge <igorjplino@gmail.com>
2023-09-25 15:47:22 +0100

    new endpoints to Create and GetById

---
+++ src/GoFit.Api/Endpoints/WorkoutTracking/CreateWorkoutTrackingEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using GoFit.Application.Common;
+using GoFit.Application.EntitiesActions.WorkoutsTracking.Commands;
+
+namespace GoFit.Api.Endpoints.WorkoutTracking;
+
+public class CreateWorkoutTrackingEndpoint :
+    BaseEndpoint<CreateWorkoutTrackingCommand, Guid>
+{
+    public override void Configure()
+    {
+        Post("WorkoutTracking");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CreateWorkoutTrackingCommand req, CancellationToken ct)
+    {
+        Result<Guid> result = await Mediator.Send(req, ct);
+
+        await HandleResultResponse(result, ct);
+    }
+}
---
+++ src/GoFit.Api/Endpoints/WorkoutTracking/GetWorkoutTrackingByIdEndpoint .cs
@@ -0,0 +1,22 @@
+﻿using GoFit.Application.Common;
+using GoFit.Application.EntitiesActions.WorkoutsTracking.Dtos;
+using GoFit.Application.EntitiesActions.WorkoutsTracking.Queries;
+
+namespace GoFit.Api.Endpoints.WorkoutTracking;
+
+public class GetWorkoutTrackingIdEndpoint :
+    BaseEndpoint<GetWorkoutTrackingByIdQuery, WorkoutTrackingDto?>
+{
+    public override void Configure()
+    {
+        Get("WorkoutTracking/{id}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(GetWorkoutTrackingByIdQuery req, CancellationToken ct)
+    {
+        Result<WorkoutTrackingDto?> result = await Mediator.Send(req, ct);
+
+        await HandleResultResponse(result, ct);
+    }
+}
