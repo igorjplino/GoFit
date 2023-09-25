b24bed20160f9268ea484763628beeac2e814a64
Igor Jorge <igorjplino@gmail.com>
2023-09-25 15:45:16 +0100

    DbContexts configurations of the new entities

---
+++ src/GoFit.Infrastructure/Contexts/Config/WorkoutSetTrackingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using GoFit.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GoFit.Infrastructure.Contexts.Config;
+
+public class WorkoutSetTrackingConfiguration : IEntityTypeConfiguration<WorkoutSetTracking>
+{
+    public void Configure(EntityTypeBuilder<WorkoutSetTracking> builder)
+    {
+        builder.ToTable("WorkoutSetTracking");
+
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.Id)
+            .ValueGeneratedOnAdd();
+    }
+}
---
+++ src/GoFit.Infrastructure/Contexts/Config/WorkoutTrackingConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using GoFit.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GoFit.Infrastructure.Contexts.Config;
+
+public class WorkoutTrackingConfiguration : IEntityTypeConfiguration<WorkoutTracking>
+{
+    public void Configure(EntityTypeBuilder<WorkoutTracking> builder)
+    {
+        builder.ToTable("WorkoutTracking");
+
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.HasOne(o => o.Workout)
+            .WithMany()
+            .HasForeignKey(o => o.WorkoutId)
+            .IsRequired();
+
+        builder.HasMany(o => o.Sets)
+            .WithOne(o => o.WorkoutTracking)
+            .HasForeignKey(o => o.WorkoutTrackingId)
+            .IsRequired();
+    }
+}
