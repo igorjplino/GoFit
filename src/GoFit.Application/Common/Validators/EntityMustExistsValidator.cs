93bb7cd9d3b4a7cbcf58788907de54008a41aca8
Igor Jorge <igorjplino@gmail.com>
2023-11-16 16:01:12 +0000

    Created common validator to check if entities exists

---
+++ src/GoFit.Application/Common/Validators/EntityMustExistsValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using GoFit.Application.Interfaces;
+using GoFit.Domain.Common;
+
+namespace GoFit.Application.Common.Validators;
+public class EntityMustExistsValidator<T> : AbstractValidator<Guid> where T : BaseEntity
+{
+    public EntityMustExistsValidator(IBaseRepository<T> baseRepository)
+    {
+        RuleFor(x => x)
+            .MustAsync(async (id, ct) =>
+            {
+                T? entity = await baseRepository.GetAsync(id);
+
+                return entity is not null;
+            });
+    }
+}
