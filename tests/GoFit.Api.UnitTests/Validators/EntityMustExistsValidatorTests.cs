d5d2a60335f9ef54306adb38e74ab6160a05baa5
Igor Jorge <igorjplino@gmail.com>
2023-11-17 18:30:06 +0000

    Added unit tests for EntityMustExistsValidator

---
+++ tests/GoFit.Api.UnitTests/Validators/EntityMustExistsValidatorTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentValidation.TestHelper;
+using GoFit.Application.Common.Validators;
+using GoFit.Application.Interfaces;
+using GoFit.Domain.Common;
+
+namespace GoFit.Api.UnitTests.Validators;
+
+public class EntityMustExistsValidatorTests
+{
+    private readonly EntityMustExistsValidator<BaseEntity> _validator;
+
+    private readonly MockRepository _mockRepository;
+    private readonly Mock<IBaseRepository<BaseEntity>> _baseRepositoryMock;
+
+    public EntityMustExistsValidatorTests()
+    {
+        _mockRepository = new MockRepository(MockBehavior.Loose);
+
+        _baseRepositoryMock = _mockRepository.Create<IBaseRepository<BaseEntity>>();
+
+        _validator = new EntityMustExistsValidator<BaseEntity>(_baseRepositoryMock.Object);
+    }
+
+    [Fact]
+    [Trait("BaseEntity", "Id")]
+    public async Task WhenEntityDoesNotExists_ShouldFail()
+    {
+        var entityId = It.IsAny<Guid>();
+
+        _baseRepositoryMock
+            .Setup(x => x.GetAsync(entityId))
+            .ReturnsAsync((BaseEntity?)null);
+
+        var result = await _validator.TestValidateAsync(entityId);
+
+        result.ShouldHaveValidationErrorFor(x => x);
+    }
+
+    [Fact]
+    [Trait("BaseEntity", "Id")]
+    public async Task WhenEntityExists_ShouldNotFail()
+    {
+        var entityId = It.IsAny<Guid>();
+
+        var mockBaseEntity = _mockRepository.Create<BaseEntity>();
+
+        _baseRepositoryMock
+            .Setup(x => x.GetAsync(entityId))
+            .ReturnsAsync(mockBaseEntity.Object);
+
+        var result = await _validator.TestValidateAsync(entityId);
+
+        result.ShouldNotHaveValidationErrorFor(x => x);
+    }
+}
