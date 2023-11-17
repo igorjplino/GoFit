1987bf3d1439eb68ca8e1a280167c57f1c753a8c
Igor Jorge <igorjplino@gmail.com>
2023-11-17 18:28:49 +0000

    Added validation for WorkoutTracking

---
+++ src/GoFit.Application/EntitiesActions/WorkoutsTracking/Validators/CreateWorkoutTrackingCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using GoFit.Application.Common.Validators;
+using GoFit.Application.EntitiesActions.WorkoutsTracking.Commands;
+using GoFit.Application.Interfaces;
+using GoFit.Domain.Entities;
+
+namespace GoFit.Application.EntitiesActions.WorkoutsTracking.Validators;
+public class CreateWorkoutTrackingCommandValidator : AbstractValidator<CreateWorkoutTrackingCommand>
+{
+    public CreateWorkoutTrackingCommandValidator(IWorkoutRepository workoutRepository)
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(x => x.WorkoutId)
+            .SetValidator(new EntityMustExistsValidator<Workout>(workoutRepository));
+
+        RuleFor(x => x.Note)
+            .MaximumLength(300).When(x => x.Note is not null);
+
+        RuleForEach(x => x.Sets)
+            .SetValidator(new WorkoutSetTrackingDtoValidator());
+    }
+}
---
+++ src/GoFit.Application/EntitiesActions/WorkoutsTracking/Validators/WorkoutSetTrackingDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using GoFit.Application.EntitiesActions.WorkoutsTracking.Dtos;
+
+namespace GoFit.Application.EntitiesActions.WorkoutsTracking.Validators;
+public class WorkoutSetTrackingDtoValidator : AbstractValidator<WorkoutSetTrackingDto>
+{
+    public WorkoutSetTrackingDtoValidator()
+    {
+        RuleFor(x => x.Repetitions)
+            .NotEmpty()
+            .GreaterThan(0);
+    }
+}
