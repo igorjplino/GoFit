7bcbe309586432e873a3f00fe4efb7073a992de3
Igor Jorge <igorjplino@gmail.com>
2023-11-17 18:29:38 +0000

    Added unit tests for WorkoutTracking

---
+++ tests/GoFit.Api.UnitTests/Validators/WorkoutsTracking/WorkoutSetTrackingDtoValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation.TestHelper;
+using GoFit.Application.EntitiesActions.WorkoutsTracking.Dtos;
+using GoFit.Application.EntitiesActions.WorkoutsTracking.Validators;
+
+namespace GoFit.Api.UnitTests.Validators.WorkoutsTracking;
+public class WorkoutSetTrackingDtoValidatorTests
+{
+    private readonly WorkoutSetTrackingDtoValidator _validator;
+
+    public WorkoutSetTrackingDtoValidatorTests()
+    {
+        _validator = new WorkoutSetTrackingDtoValidator();
+    }
+
+    [Fact]
+    [Trait("WorkoutSetTrackingDto", "Note")]
+    public void WhenIsEqualOrLessThan0Chars_ShouldFail()
+    {
+        var dto = new WorkoutSetTrackingDto
+        {
+            Repetitions = 0
+        };
+
+        var result = _validator.TestValidate(dto);
+
+        result.ShouldHaveValidationErrorFor(x => x.Repetitions);
+    }
+
+    [Fact]
+    [Trait("WorkoutSetTrackingDto", "Note")]
+    public void WhenIsGreaterThan0Chars_ShouldNotFail()
+    {
+        var dto = new WorkoutSetTrackingDto
+        {
+            Repetitions = 1
+        };
+
+        var result = _validator.TestValidate(dto);
+
+        result.ShouldNotHaveValidationErrorFor(x => x.Repetitions);
+    }
+}
