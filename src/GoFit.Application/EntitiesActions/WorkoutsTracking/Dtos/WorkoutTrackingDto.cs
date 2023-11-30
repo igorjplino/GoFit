7f0d60069bd3ebb81777bf6d81b4597d993fefae
Igor Jorge <igorjplino@gmail.com>
2023-11-30 14:18:53 +0000

    Note property on WorkoutSetTrackingDto accepting null

--- src/GoFit.Application/EntitiesActions/WorkoutsTracking/Dtos/WorkoutTrackingDto.cs
+++ src/GoFit.Application/EntitiesActions/WorkoutsTracking/Dtos/WorkoutTrackingDto.cs
@@ -7,7 +7,7 @@ public record WorkoutTrackingDto
     public Guid WorkoutId { get; set; }
     public WorkoutDto Workout { get; set; }
     public DateTime StartWorkoutDate { get; set; }
-    public DateTime EndWorkoutDate { get; set; }
+    public DateTime? EndWorkoutDate { get; set; }
     public string? Note { get; set; }
     public ICollection<WorkoutSetTrackingDto> Sets { get; set; }
 }
