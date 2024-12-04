83c6abb3da4d6f769f7ebb3f99c6f22b46e53774
Igor Jorge <igorjplino@gmail.com>
2024-12-04 18:01:52 +0000

    Implemented not-found component

--- src/client/src/app/app.routes.ts
+++ src/client/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ExerciseComponent } from './features/exercise/exercise.component';
 import { ExerciseDetailsComponent } from './features/exercise-details/exercise-details.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'exercise', component: ExerciseComponent },
     { path: 'exercise/:id', component: ExerciseDetailsComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' }
+    { path: 'not-found', component: NotFoundComponent },
+    { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
