a82bcec2e10aac0a5ed6f97019e361ea025ffc3e
Igor Jorge <igorjplino@gmail.com>
2024-12-04 18:09:11 +0000

    Added loading inerceptor

--- src/client/src/app/app.config.ts
+++ src/client/src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient()
+    provideHttpClient(withInterceptors([
+      loadingInterceptor
+    ])),
   ]
 };
