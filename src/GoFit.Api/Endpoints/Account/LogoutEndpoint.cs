516611f0e0cd16df5ca5d86124439f201727e551
Igor Jorge <igorjplino@gmail.com>
2025-01-20 18:00:50 +0000

    Added Logou endpoint

---
+++ src/GoFit.Api/Endpoints/Account/LogoutEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using FastEndpoints;
+using GoFit.Api.Endpoints.Account.Validators;
+
+namespace GoFit.Api.Endpoints.Account;
+
+public class LogoutEndpoint : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Post("Account/Logout");
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        HttpContext.Response.Cookies.Delete("access_token");
+        await SendNoContentAsync();
+    }
+}
